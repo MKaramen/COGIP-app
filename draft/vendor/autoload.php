af820f3ce1416e1795cddebfef8314d5e9dfdf95
julio ZINGA <54060896+julio-34727@users.noreply.github.com>
2019-10-31 14:55:45 +0100

    update autoload php file

---
+++ draft/vendor/autoload.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+
+spl_autoload_register(function ($classname) {
+    $folders = array(
+        $_SERVER['DOCUMENT_ROOT'] . '/app/classes/'     . $classname . '.php', 
+        $_SERVER['DOCUMENT_ROOT'] . '/app/route/'       . $classname . '.php', 
+        $_SERVER['DOCUMENT_ROOT'] . '/app/controllers/' . $classname . '.php', 
+        $_SERVER['DOCUMENT_ROOT'] . '/app/models/'      . $classname . '.php', 
+        $_SERVER['DOCUMENT_ROOT'] . '/app/views/'       . $classname . '.php',
+    );
+    foreach ($folders as $filename) {
+        $filename = str_replace('\\', '/', $filename);
+        if (file_exists($filename)) require_once $filename;
+    }
+});
