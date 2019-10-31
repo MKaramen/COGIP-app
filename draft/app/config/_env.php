4dc162a86cc675b6145b0b4c55324e96de51cf4d
julio ZINGA <54060896+julio-34727@users.noreply.github.com>
2019-10-31 15:07:19 +0100

    update _env php file

---
+++ draft/app/config/_env.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+
+/* Declare const of arrays */
+define('APP_PARAMS', array(
+
+    # App parameters
+    'APP_URL'  => 'http://localhost:3000/cogip/',
+    'APP_ROOT' => str_replace('\\', '/', $_SERVER['DOCUMENT_ROOT']),
+    'APP_ENV'  => 'local',
+    'APP_NAME' => 'cogip MVC app',
+
+    # Database parameters
+    'DB_DRIVER'   => 'mysql',     # Data source name (DSN)
+    'DB_HOST'     => 'localhost', # Database host
+    'DB_NAME'     => 'becode',    # Database name
+    'DB_USERNAME' => 'root',      # Database user
+    'DB_PASSWORD' => '',          # Database password
+
+    # PHP config
+
+));
+
+/* set ENV variables */
+foreach(APP_PARAMS as $key => $value) {
+    putenv($key . '=' . $value);
+}
