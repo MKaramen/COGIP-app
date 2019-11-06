2e544ce91b6afecb4e14e03e72e38e6ec8f6fdce
julio ZINGA <54060896+julio-34727@users.noreply.github.com>
2019-11-06 10:05:14 +0100

    update app.js file

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,10 +1,30 @@
+/* === Vanilla JS === */
+
+/* Hamburger icon */ 
+document.querySelector('.nav__hamburger').addEventListener('click', event => {
+    event.target.classList.toggle('change');
+});
+
+/* Show or hide password */
+document.querySelector('.login .toggle-password').addEventListener('click', event => {
+    const $password = document.querySelector('.login [id="password"]');
+    if ($password.getAttribute('type') === 'password') {
+        event.target.classList.remove('fa-eye-slash');
+        event.target.classList.add('fa-eye');
+        $password.setAttribute('type', 'text');
+    }
+    else {
+        event.target.classList.remove('fa-eye');
+        event.target.classList.add('fa-eye-slash');
+        $password.setAttribute('type', 'password');
+    }
+});
+
+
 /* === jQuery === */
-$(document).ready(function() {
-    // Scripts for hamburger icon 
-    $('.nav__hamburger').click(function() {
-        $('.nav__hamburger').toggleClass('change');
-    });
-    
-
-
-});  
+// $(document).ready(function() {
+//     // Scripts for hamburger icon 
+//     $('.nav__hamburger').click(function() {
+//         $('.nav__hamburger').toggleClass('change');
+//     });
+// }); 
