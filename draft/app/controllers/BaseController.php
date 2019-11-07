7b7af772c05d9bd7fffad1712b3069f859814eff
kibens <j.tusamba@gmail.com>
2019-11-07 12:16:31 +0100

    update all files

--- draft/app/controllers/BaseController.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once $_SERVER['DOCUMENT_ROOT'] . '/COGIP-app/draft/app/config/_env.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/COGIP-app/draft/vendor/autoload.php';
-
-$page = "company.php";
-
-var_dump(BaseModel::displayPage($page));
