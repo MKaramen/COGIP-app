a7d162aeb73ade412f0e7ed4776ff892fd3f3541
Zhuang <zhuangyongchin@gmail.com>
2019-11-08 10:16:46 +0100

    update country

--- app/views/inc/arrayCountry.php
+++ app/views/inc/arrayCountry.php
@@ -1,11 +1,11 @@
 <?php
 
 $continents = [
+    'Europe' => array('Belgium','France','Germany','Italy','Luxembourg','Monaco','Netherlands','Russia','Switzerland','United Kingdom'),
+
     'Africa' =>  array('Algeria','Burkina Faso','Cameroon','Central African Republic','Democratic Republic of the Congo','Republic of the Congo','South Africa',),
     
     'Asia' => array('Bangladesh','Cambodia','China','India','Indonesia','Japan',),
-    
-    'Europe' => array('Belgium','France','Germany','Italy','Luxembourg','Monaco','Netherlands','Russia','Switzerland','United Kingdom'),
     
     'North America' => array('Bahamas','Canada','United States of America'),
    
