6156a1cc4716aa0c60ed3024d01eadda405a8df2
julio ZINGA <54060896+julio-34727@users.noreply.github.com>
2019-11-11 14:39:11 +0100

    update Helper class

--- app/helpers/Helper.php
+++ app/helpers/Helper.php
@@ -1,90 +1,72 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 
 /** 
-* Descritpion
+* Helper class
 */
 class Helper 
 {
 
-    public static function view(string $view, array $data= []): void
+    public static function view(string $view, array $dataInfo, array $dataModel): void
     {
-        // Require view file
-        $path = getenv('APP_ROOT') . '/app/views/' . $view . '.php';
+        $file = getenv('APP_ROOT') . '/app/views/' . $view . '.php';
 
-        if(!file_exists($path)) throw new Exception($path . 'not found!');
+        if(!file_exists($file)) throw new Exception($file . ' is not found!');
+        if(!is_readable($file)) throw new Exception($file . ' is not readable!');
 
-        require_once $path;
+        require_once $file;
     }
 
     /* Redirect to specific page */
-    public static function to(string $page): void 
-    {
-        header('location: ' . $page);
-    }
+    public static function to(string $page): void {exit(header('location: ' . getenv('APP_URL') . $page));}
     
     /* Redirect to same page */
-    public static function back(): void 
+    public static function back(): void {exit(header('location: ' . $_SERVER['REQUEST_URI']));}
+
+    /* Static method to var dump with pre */ 
+    public static function dump($vars): void
     {
-        $uri = $_SERVER['REQUEST_URI'];
-        header('location: ' . $uri);
+        echo '<pre>';
+        var_dump($vars);
+        echo '</pre>';
     }
-}
 
+    /* Get all $_POST from the form page */
+    public static function getPost(): array {return (isset($_POST)) ? $_POST : [];}
 
-    // public static function baseURL(string $page): string
-    // {
-    //     return self::getAppURL() . '/' . $page;
-    // }
+    /* Uppercase the first letter/s of a string, including where the names are joined by a hyphens)*/
+    public static function capitalize(string $word): string 
+    {
+        return implode('-', array_map('ucfirst', explode('-', ucwords($word))));
+    }
 
-    // public static function siteURL(string $page): string
-    // {
-    //     return self::getAppURL() . '/index.php/' . $page;
-    // }
-
-    /* Returns post request data */
-    // public static function postRequest(bool $is_array=true): array 
-    // {
-    //     $result = array();
-    //     if (count($_GET) > 0)  $result['get'] = $_GET;
-
-    //     return json_decode(json_encode($result), $is_array);
-    // }
-
-    // public static function getRequest(bool $is_array=true): array 
-    // {
-    //     $result = array();
-    //     if (count($_POST) > 0) $result['post'] = $_POST;
-    //     $result['file'] = $_FILES;
-    //     return json_decode(json_encode($result), $is_array);
-    // }
-
-    // public static function fileRequest(bool $is_array=true): array 
-    // {
-    //     $result = array();
-    //     if (count($_GET) > 0)  $result['get'] = $_GET;
-    //     if (count($_POST) > 0) $result['post'] = $_POST;
-    //     $result['file'] = $_FILES;
-    //     return json_decode(json_encode($result), $is_array);
-    // }
-
-    /**
-     * Returns a specific request data
+    /** Create a table, make every single element clickable and 
+     * it redirects the user to the right detail page 
      */
-    // public static function get(string $key): array {
-    //     $obj = new static();
-    //     $data = $obj->all(true);
-    //     return $data[$key];
-    // }
+    public static function makeTable(string $categoryName, array $dataModel): void
+    {
+        # Heading (<thead>)
+        // echo '<thead class="text-center">';
+        // foreach ($dataModel[$categoryName]['col'] as $colName) 
+        // {
+        //     echo '<tr><th scope="col">' . $colName . '</th></tr>';
+        // }
+        // echo '</thead>';
+
+        # Body (<tbody>)
+        // echo '<tbody>';
+        // foreach ($dataModel[$categoryName]['row'] as $user) 
+        // {
+        //     foreach ($user as $key => $value) 
+        //     {
+        //         echo '<tr><td>' . $value . '</td></tr>';
+
+        //         // Create all the col with the values and cliclable links
+        //     }
+        // }
+        // echo '</tbody>';
+    }
 
 
-    /* Get the app URL dynamically */
-    // function getAppURL(): string
-    // {
-    //     if(isset($_SERVER['HTTPS'])) $protocol = ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] != 'off') ? 'https' : 'http';
-    //     else $protocol = 'http';
 
-    //     return rtrim($protocol . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], '/');
-    // }
+}
