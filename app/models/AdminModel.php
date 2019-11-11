138fbab2b6420e749b72169140f9b8febb4a9b54
julio ZINGA <54060896+julio-34727@users.noreply.github.com>
2019-11-11 14:43:00 +0100

    update AdminModel class

--- app/models/AdminModel.php
+++ app/models/AdminModel.php
@@ -1,6 +1,4 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 
 /*
@@ -8,9 +6,27 @@ declare(strict_types=1);
  */
 class AdminModel extends Model
 {
-    public function dashboard()
+    /* Check if the username (people_lastname) is in the database */
+    public function isUserFound(string $username): bool
     {
+        $this->open();
+        $userQuery = $this->db->prepare("SELECT * FROM people WHERE people_lastname=:people_lastname");
+        $userQuery->execute(array('people_lastname' => $username));
+        $this->close();
 
+        return boolval($userQuery->rowCount());
     }
-    
+
+    /* Check if user is logged in and returns user info  */
+    public function login(string $username, string $password): array
+    {
+        $this->open();
+        $userQuery = $this->db->prepare("SELECT * FROM people WHERE people_lastname=:people_lastname");
+        $userQuery->execute(array('people_lastname' => $username));
+        $user = $userQuery->fetch();
+        $passwordHashed = $user['people_password'] ? $user['people_password'] : ''; // prevent errors (if returns FALSE)
+        $this->close();
+
+        return password_verify($password, $passwordHashed) ? $user : array();
+    }
 }
