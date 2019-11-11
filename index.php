a89d8503de2360ee3fc433fd9adb8474078b05ae
julio ZINGA <54060896+julio-34727@users.noreply.github.com>
2019-11-11 15:08:34 +0100

    update index php file

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php 
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 
 /**
@@ -9,26 +7,10 @@ declare(strict_types=1);
  *   - load the config file (.env.php)
  *   - setup autoloader (autoload.php)
  */
-#if(!isset($_SESSION)) session_start();
+if(!isset($_SESSION)) session_start();
 require_once './app/config/.env.php';
 require_once './vendor/autoload.php';
 
 /* Setup routing */
 $route = new Route();
 $route->routeRequest();
-
-
-
-
-
-
-
-/* instantiate database class */
-#new \App\Classes\Database();
-
-## set custom error handler
-//set_error_handler([new \App\Classes\ErrorHandler(), 'handleErrors']);
-
-//require_once __DIR__ . '/../app/routing/routes.php';
-
-//new \App\RouteDispatcher($router);
