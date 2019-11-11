b7523e667cdfc1b0220565ca833f96b9b6386108
julio ZINGA <54060896+julio-34727@users.noreply.github.com>
2019-11-11 15:03:54 +0100

    update views pages files

--- app/views/pages/users.php
+++ app/views/pages/users.php
@@ -1,19 +1,26 @@
-<!-- header content -->
 <?php require getenv('APP_ROOT') . '/app/views/inc/header.php'?>
 
-<!-- main content -->
+<!-- MAIN (index.php) -->
 <main id="main" class="jumbotron jumbotron-fluid mb-0 py-3">
-    <!-- main-content -->
-    <section class="container content">
-        <h2 class="content__title"><?=$data['content_title']?></h2>
+
+    <!-- ASIDE (dashboard) -->
+    <aside id="aside" class="dashboard">
+
+    </aside>
+    <!-- END OF ASIDE -->
+
+    <!-- MAIN CONTENT (users.php) -->
+    <section id="home" class="container content">
+        <h2 class="content__title"><?=$dataInfo['content_title']?></h2>
         <div class="content__body">
-            <p class="content__description"><?=$data['content_description']?></p>
-
-            <!-- Tables -->
-
+            <p class="content__description"><?=$dataInfo['content_description']?></p>
         </div>
-    </section>
-</main>
 
-<!-- footer content -->
+    </section> 
+    <!-- END OF MAIN CONTENT -->
+
+</main>
+<!-- END OF MAIN -->
+
 <?php require getenv('APP_ROOT') . '/app/views/inc/footer.php'?>
+<!-- END OF BODY -->
