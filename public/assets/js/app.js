4c58ef947d9708c40701b9feaae001f9116703fd
julio ZINGA <54060896+julio-34727@users.noreply.github.com>
2019-11-12 14:47:02 +0100

    update app.js file

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,25 @@
+/* === FUNCTIONS === */
+
+const setupTable = ($node) => {
+    $node.DataTable({
+        'pageLength': 3,
+        'lengthMenu': [[3,5,10,-1], [3,5,10,'All']],
+        'ordering': true,
+        'searching': true,
+        'paging': true,
+        'responsive': true,
+        'dom': 'Blfrtip',
+        buttons: [
+            {
+                text: 'Add',
+                action: function (e, dt, node, config ) {
+                    alert('Button activated');
+                }
+            }
+        ]
+    });
+}
+
 /* === jQUERY === */
 
 $(document).ready(function() {
@@ -9,12 +31,15 @@ $(document).ready(function() {
     $('.password').click(() => {
         const $password = $('[id="password"]');
         const $icon = $('.password__toggle');
-        if ($password.attr('type') === 'password') {
+
+        if ($password.attr('type') === 'password') 
+        {
             $icon.removeClass('fa-eye-slash');
             $icon.addClass('fa-eye');
             $password.attr('type', 'text');
         }
-        else {
+        else 
+        {
             $icon.removeClass('fa-eye');
             $icon.addClass('fa-eye-slash');
             $password.attr('type', 'password');
@@ -24,8 +49,14 @@ $(document).ready(function() {
     /* SIDEBAR (show or hide on mouse over/out) */
     $('#nav-sidebar .nav-link').hover(
         () => $('#nav-sidebar .nav-link span').removeClass('d-none'),
-        () => $(window).width() < 992 ? $('#nav-sidebar .nav-link span').addClass('d-none') : false
+        () => $(window).width() < 1140 ? $('#nav-sidebar .nav-link span').addClass('d-none') : false
     );
+
+    /* ADMIN TABLES */
+    setupTable($('#admin__users'));
+    setupTable($('#admin__companies'));
+    setupTable($('#admin__invoices'));
+    $('.dt-button').addClass('btn');
 
 
 
